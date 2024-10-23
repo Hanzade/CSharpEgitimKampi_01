4d5f5e1afa57481a733c5c7aa75dea699e389b53
Hanzade <beyzaozturk1705@gmail.com>
2024-10-23 12:08:08 +0300

    04_Loops

--- 03_MakingDecision/Program.cs
+++ 03_MakingDecision/Program.cs
@@ -119,32 +119,32 @@ namespace _03_MakingDecision
 
             #region Mod İşlemleri
 
-            int number;
-            number = 26;
-            int sonuc = number % 5;
-            Console.WriteLine(sonuc);
+            //int number;
+            //number = 26;
+            //int sonuc = number % 5;
+            //Console.WriteLine(sonuc);
 
-            Console.Write("Lütfen 1. sayıyı giriniz: ");
-            int number1 = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen 1. sayıyı giriniz: ");
+            //int number1 = int.Parse(Console.ReadLine());
 
-            Console.Write("Lütfen 2. sayıyı giriniz: ");
-            int number2 = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen 2. sayıyı giriniz: ");
+            //int number2 = int.Parse(Console.ReadLine());
 
 
-            int result = number1 % number2;
-            Console.WriteLine("1. sayının 2. sayıya bölümünden kalan: " + result);
+            //int result = number1 % number2;
+            //Console.WriteLine("1. sayının 2. sayıya bölümünden kalan: " + result);
 
-            Console.Write("Lütfen sayıyı giriniz: ");
-            int number = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen sayıyı giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
 
-            if (number % 2 == 0)
-            {
-                Console.WriteLine("sayi çifttir");
-            }
-            else
-            {
-                Console.WriteLine("sayi tektir. ");
-            }
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("sayi çifttir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sayi tektir. ");
+            //}
             #endregion
 
             #region CharDegiskenler
@@ -261,82 +261,80 @@ namespace _03_MakingDecision
 
             #region Switch Case
 
-            Console.WriteLine("Lütfen Ay Girişi yapınız: ");
-            int monthNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Lütfen Ay Girişi yapınız: ");
+            //int monthNumber = int.Parse(Console.ReadLine());
 
-            switch (monthNumber)
-            {
-                case 1:
-                    Console.WriteLine("Ocak");
-                    break;
-                case 2:
-                    Console.WriteLine("Şubat");
-                    break;
-                case 3:
-                    Console.WriteLine("Mart");
-                    break;
-                case 4:
-                    Console.WriteLine("Nisan");
-                    break;
-                case 5:
-                    Console.WriteLine("Mayıs");
-                    break;
-                case 6:
-                    Console.WriteLine("Haziran");
-                    break;
-                case 7:
-                    Console.WriteLine("Temmuz");
-                    break;
-                case 8:
-                    Console.WriteLine("Ağustos");
-                    break;
-                case 9:
-                    Console.WriteLine("Eylül");
-                    break;
-                case 10:
-                    Console.WriteLine("Ekim");
-                    break;
-                case 11:
-                    Console.WriteLine("Kasım");
-                    break;
-                case 12:
-                    Console.WriteLine("Aralık");
-                    break;
-                default:
-                    Console.WriteLine("hatalı veri girişi");
-                    break;
+            //switch (monthNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Ocak");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Şubat");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Mart");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Nisan");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Mayıs");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Haziran");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Temmuz");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Ağustos");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Eylül");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Ekim");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Kasım");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Aralık");
+            //        break;
+            //    default:
+            //        Console.WriteLine("hatalı veri girişi");
+            //        break;
 
-            }
+            //}
 
             #endregion
 
 
             #region HesapMakinesi
 
-            int number1, number2, result;
-            char symbol;
+            //int number1, number2, result;
+            //char symbol;
 
-            Console.WriteLine("1. sayıyı giriniz: ");
-            number1 = int.Parse(Console.ReadLine());
-            number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("1. sayıyı giriniz: ");
+            //number1 = int.Parse(Console.ReadLine());
+            //number2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Lütfen yapmak istediğiniz işlemi giriniz:");
-            symbol = char.Parse(Console.ReadLine());
-            switch (symbol)
-            {
-                case '+':
-                    result = number1 + number2; break;
-                case '-':
-                    result = number1 - number2; break;
-                case '*':
-                    result = number1 * number2; break;
-                case '/':
-                    result = number1 / number2; break;
-
-            }
+            //Console.WriteLine("Lütfen yapmak istediğiniz işlemi giriniz:");
+            //symbol = char.Parse(Console.ReadLine());
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result = number1 + number2; break;
+            //    case '-':
+            //        result = number1 - number2; break;
+            //    case '*':
+            //        result = number1 * number2; break;
+            //    case '/':
+            //        result = number1 / number2; break;
 
             #endregion
-            Console.Read();
         }
+
     }
 }
---
+++ 04_Loops/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ForDöngüsü
+
+            ////For(x;y;z)
+            //// x: döngünün başlangıç değeri
+            ////y: bitiş
+            ////z: artış ya da azalış değeri
+
+
+            //int i;
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C eğitim kampı ");
+            //}
+
+
+            //for (int i = 3; i<=50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine("Ekrana yazılmasını istediğiniz adedi giriniz:");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+            #endregion
+
+            #region ForDongusuIleKararYapıları
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //int totalValue = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    totalValue += i;
+            //    Console.WriteLine(i + " " + totalValue);
+            //}
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0; //1 ile 20 arasındaki çift sayıların 
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //    }
+            //}
+            //Console.WriteLine(totalValue);
+
+            //1 ile 50 arasında 7 ye tam bölünen kaç sayı var 
+
+            //int i = 0;
+            //int count = 0;
+            //for (i = 1; i < 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count += 1;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //bir bakteri türü her saatin sonunda kendini 2ye bölerek üremektedir. //1-2-4-8-16 //her gelen de bölünüyor
+
+            //int bacterium = 1;
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ". saat sonunda " + bacterium);
+
+            //}
+            #endregion
+
+            #region WhileDöngüsü
+
+            //şart sağlandığı müddetçe
+
+            //While(Şart)
+            //{
+            //İşlemler
+            //}
+
+            //int i = 1;
+            //while(i<=10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler..");
+            //    i++;
+            //}
+
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+
+            //int i = 1;
+            //int sum = 0;
+            //while (i<=10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+
+
+            #endregion
+
+            #region ÖrnekSınavSorusu
+
+            //klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+
+            //356 = 356 / 100 bölüm: 3 kalan 56 / 10: bölüm 5, kalan 6
+
+            //benim yaptığım:
+
+            Console.WriteLine("lütfen 3 basamaklı bir sayı giriniz: "); //356 
+            int number = int.Parse(Console.ReadLine());
+            int yüzler, onlar, birler;
+            int kalan = 0;
+
+            yüzler = number / 100; //yüzler 3 
+            number = number % 100; //56
+            onlar = number / 10; //5 //number 56 
+            kalan = number - (onlar * 10);
+            birler = kalan;
+
+            Console.WriteLine(yüzler + onlar + birler);
+
+
+
+
+            Console.Write("sayıyı giriniz:");
+
+            int number1 = int.Parse(Console.ReadLine());
+            int ones, tens, hundres;
+            int sum;
+
+            ones = number1 % 10; //birler
+            tens = (number1 % 100) / 10;
+            hundres = number1 / 100; // yüzler
+
+
+            sum = ones + hundres + tens;
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
